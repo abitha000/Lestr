d46cec76f62b9528fe9c39e0bb43e5f581a54c5d
abitha000 <abithak000@yahoo.com>
2025-07-19 21:04:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,10 +3,10 @@ import os
 class temp(object):
     BOT = None
 
-API_ID = int(os.getenv('API_ID'))
-API_HASH = os.getenv('API_HASH')
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+API_ID = int(os.getenv('API_ID', 24401235))
+API_HASH = os.getenv('API_HASH', "149f7e13d7d861b27cffc3ab1fd52b22")
+BOT_TOKEN = os.getenv('BOT_TOKEN', "8167215119:AAH9giJCHz2TGbykSnLcoy5IK1rTNrr6lkc")
 
 PORT = int(os.getenv('PORT', 8080))
-BIN_CHANNEL = int(os.getenv("BIN_CHANNEL"))
-STREAM_URL = os.getenv("STREAM_URL")
+BIN_CHANNEL = int(os.getenv("BIN_CHANNEL", -1002860251103))
+STREAM_URL = os.getenv("STREAM_URL", "https://causal-vania-kalaisel-339f7657.koyeb.app/")
