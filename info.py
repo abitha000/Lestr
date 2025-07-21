300c11449ebcaf74fac119f25c02679ce65cf0ae
abitha000 <abithak000@yahoo.com>
2025-07-21 09:02:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,6 +9,7 @@ def safe_int(val, default):
         print(f"[WARN] Cannot convert {val!r} to int; using default {default}")
         return default
 API_ID = safe_int(os.getenv('API_ID'), 24401235)
+API_HASH = os.getenv('API_HASH', "149f7e13d7d861b27cffc3ab1fd52b22")
 PORT = safe_int(os.getenv('PORT'), 8080)
 BIN_CHANNEL = safe_int(os.getenv('BIN_CHANNEL'), -1002860251103)
 BOT_TOKEN = os.getenv('BOT_TOKEN', "8167215119:AAH9giJCHz2TGbykSnLcoy5IK1rTNrr6lkc")
